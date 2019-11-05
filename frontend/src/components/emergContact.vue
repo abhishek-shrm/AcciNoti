7c380d804885600ba7532fd84462feebb12673d1
abhishek-shrm <abhishek.shrm.2195@gmail.com>
2019-11-06 04:23:40 +0530

    emergency contact added

--- frontend/src/components/emergContact.vue
+++ frontend/src/components/emergContact.vue
@@ -1,8 +1,58 @@
 <template>
   <section>
     <div class="block">
-      <h1 class="title is-3">Emergency Contact</h1>
+      <h1 class="title is-3">Emergency Contacts</h1>
     </div>
+    <table class="table is-striped is-bordered">
+      <thead>
+        <tr>
+          <th><strong>Service</strong></th>
+          <th><strong>Contact No.</strong></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><p>NATIONAL EMERGENCY NUMBER</p></td>
+          <td><p><strong>112</strong></p></td>
+        </tr>
+        <tr>
+          <td><p>POLICE</p></td>
+          <td><p><strong>100</strong></p></td>
+        </tr>
+        <tr>
+          <td><p>FIRE</p></td>
+          <td><p><strong>101</strong></p></td>
+        </tr>
+        <tr>
+          <td><p>AMBULANCE</p></td>
+          <td><p><strong>102</strong></p></td>
+        </tr> 
+        <tr>
+          <td><p>Disaster Management Services</p></td>
+          <td><p><strong>108</strong></p></td>
+        </tr>
+        <tr>
+          <td><p>Women Helpline</p></td>
+          <td><p><strong>1091</strong></p></td>
+        </tr>
+        <tr>
+          <td><p>Domestic Abuse</p></td>
+          <td><p><strong>181</strong></p></td>
+        </tr>
+        <tr>
+          <td><p>Aids Helpline</p></td>
+          <td><p><strong>1097</strong></p></td>
+        </tr>            
+        <tr>
+          <td><p>Anti Poison New Delhi</p></td>
+          <td><p><strong>1066 or 011-1066</strong></p></td>
+        </tr>  
+        <tr>
+          <td><p>LPG Leak Helpline</p></td>
+          <td><p><strong>1906</strong></p></td>
+        </tr>                                                  
+      </tbody>
+    </table>     
   </section>
 </template>
 
@@ -33,7 +83,10 @@ export default {
     margin-top: 2em;
     text-align: center;
   }
-
+  table{
+    margin:10px 2em;
+    text-align:justify;
+  }
   @import "~bulma";
   @import "~buefy/src/scss/buefy";
 </style>
